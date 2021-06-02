8577bbb7c61ba3fc74c79b7f5ff008de349893d0
Taha Jalili <tahajalili@gmail.com>
2021-06-02 23:27:15 +0400

    new main.dart

--- TodoApp-1/lib/main.dart
+++ TodoApp-1/lib/main.dart
@@ -1,7 +1,17 @@
+
+
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(MyApp());
+}
+
+class Task {
+  final String title;
+  final String description;
+
+  Task(this.title, this.description);
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +26,7 @@ class MyApp extends StatelessWidget {
       home: MyTaskList(),
     );
   }
+
 }
 
 class MyTaskList extends StatefulWidget {
@@ -25,8 +36,7 @@ class MyTaskList extends StatefulWidget {
 }
 
 class _MyTaskListState extends State<MyTaskList> {
-  final _taskItems = <String>[];
-
+  final _taskItems = <Task>[];
   final myController = TextEditingController();
 
 
@@ -38,18 +48,18 @@ class _MyTaskListState extends State<MyTaskList> {
 
   void _addTask(String task){
     setState(() {
-      _taskItems.add(task);
+      _taskItems.add(Task(task,"Description for task: ${task}"));
     });
     myController.clear();
   }
-
 
   Widget _buildTaskList() {
     return new ListView.separated(
         itemCount: _taskItems.length,
         itemBuilder: (BuildContext context, index) {
           print(_taskItems);
-          return _buildTask(_taskItems[index],index);
+          var title = _taskItems[index].title;
+          return _buildTask(title,index);
         },
 
         separatorBuilder: (BuildContext context, int index) => const Divider(height: 3,color: Colors.black38,)
@@ -57,16 +67,25 @@ class _MyTaskListState extends State<MyTaskList> {
 
   }
 
+   newPage(index){
+    Navigator.push(context,
+        MaterialPageRoute(
+            builder: (context) => DetailScreen(task: _taskItems[index])
+        )
+    );
+  }
   Widget _buildTask(String task, index){
     if(index % 2 != 0){
       return new ListTile(
         tileColor: Colors.blueGrey[600],
         title: new Text(task, style: TextStyle(color: Colors.white, fontSize: 20),),
+        onTap: newPage(index),
       );
     } else{
       return new ListTile(
         tileColor: Colors.black38,
         title: new Text(task, style: TextStyle(color: Colors.black, fontSize: 20),),
+        onTap: newPage(index),
       );
     }
 
@@ -97,6 +116,7 @@ class _MyTaskListState extends State<MyTaskList> {
               actions: <Widget>[
                 TextButton(
                     onPressed: () {
+                      // _addTask(myController.text);
                       _addTask(myController.text);
                       Navigator.pop(context, "ok");
 
@@ -113,6 +133,27 @@ class _MyTaskListState extends State<MyTaskList> {
 
         ),
         body: _buildTaskList(),
+    );
+  }
+}
+
+
+//new page to show task detailw
+class DetailScreen extends StatelessWidget{
+  final Task task;
+
+  DetailScreen({Key? key,  required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page 2"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Text(task.description),
+      ),
     );
   }
 }
